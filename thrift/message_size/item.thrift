23b4188898884e6bd66cfea1eceefd3c8cbd9d35
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-04-03 15:30:32 +0200

    added thrift samples

---
+++ thrift/message_size/item.thrift
@@ -0,0 +1,26 @@
+struct Item {
+  1: string field1,
+  2: string field2,
+  3: string field3,
+  4: string field4,
+  5: string field5,
+  6: string field6,
+  7: string field7,
+  8: string field8,
+  9: string field9,
+  10: string field10,
+  11: string field11,
+  12: string field12,
+  13: string field13,
+  14: string field14,
+  15: string field15,
+  16: string field16,
+  17: string field17,
+  18: string field18,
+  19: string field19,
+  20: string field20,
+}
+
+struct Transaction {
+   1: list<Item> items,
+}
---
+++ thrift/message_size/sample.thrift
@@ -0,0 +1,4 @@
+struct Work {
+  1: i32 num1 = 0,
+  2: i32 num2,
+}
