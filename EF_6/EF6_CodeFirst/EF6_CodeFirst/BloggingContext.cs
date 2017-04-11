37fc4537006aab18c491b6ff26f4c6cddd0dc209
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-04-11 15:16:44 +0200

    ef6_codefirst

---
+++ EF_6/EF6_CodeFirst/EF6_CodeFirst/BloggingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Common;
+
+namespace EF6_CodeFirst
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(string connString):base(connString)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
--- EF_6/EF6_CodeFirst/EF6_CodeFirst/Program.cs
+++ EF_6/EF6_CodeFirst/EF6_CodeFirst/Program.cs
@@ -10,6 +10,22 @@ namespace EF6_CodeFirst
     {
         static void Main(string[] args)
         {
+            using (var db = new BloggingContext("Data Source=W7-PLKTW0206;Initial Catalog=testdb;Integrated Security=True"))
+            {
+                var blog = new Blog() { Name = "My Blog" };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                foreach(var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+
         }
     }
 }
