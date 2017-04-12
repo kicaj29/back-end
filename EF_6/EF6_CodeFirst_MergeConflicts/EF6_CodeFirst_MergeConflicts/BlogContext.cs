c7599fc10627a45191f82411ae8849d55e37cf53
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-04-12 13:53:06 +0200

    migration merge conflicts

---
+++ EF_6/EF6_CodeFirst_MergeConflicts/EF6_CodeFirst_MergeConflicts/BlogContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6_CodeFirst_MergeConflicts
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
---
+++ EF_6/EF6_CodeFirst_MergeConflicts/EF6_CodeFirst_MergeConflicts/Migrations/201704121152275_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace EF6_CodeFirst_MergeConflicts.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        BlogId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Url = c.String(),
+                    })
+                .PrimaryKey(t => t.BlogId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Blogs");
+        }
+    }
+}
