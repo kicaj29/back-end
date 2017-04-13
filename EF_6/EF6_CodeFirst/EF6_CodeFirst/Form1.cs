381db41aa0bc0b98cc1a07eebc150682142955e5
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-04-13 08:57:18 +0200

    converted to WinForms

--- EF_6/EF6_CodeFirst/EF6_CodeFirst/Form1.cs
+++ EF_6/EF6_CodeFirst/EF6_CodeFirst/Form1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,48 @@ namespace EF6_CodeFirst
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //1. The migrations are run even we do not run the context. Seems that this approach fits better to installers.
+            var configuration = new Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //2. The migrations are run the first time the context is used within the application process.
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BloggingContext, Migrations.Configuration>());
+
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Name = "Another Blog " });
+                db.SaveChanges();
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Name);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DecompressDatabaseMigration decompress = new DecompressDatabaseMigration();
+            decompress.DecompressModel(
+                "201704120821095_AddBlogUrl",
+                @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=EF6_CodeFirst.BloggingContext;Integrated Security=True");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DecompressDatabaseMigration decompress = new DecompressDatabaseMigration();
+            decompress.CompressModel(
+                "201704120821095_AddBlogUrl",
+                @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=EF6_CodeFirst.BloggingContext;Integrated Security=True",
+                "Model.xml");
         }
     }
 }
--- EF_6/EF6_CodeFirst/EF6_CodeFirst/Program.cs
+++ EF_6/EF6_CodeFirst/EF6_CodeFirst/Program.cs
@@ -17,33 +17,6 @@ namespace EF6_CodeFirst
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-            //Two ways to execute migrations from C# level:
-
-            //1. The migrations are run the first time the context is used within the application process.
-            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<BloggingContext, Migrations.Configuration>());
-
-            //using (var db = new BloggingContext())
-            //{
-            //    db.Blogs.Add(new Blog { Name = "Another Blog " });
-            //    db.SaveChanges();
-
-            //    foreach (var blog in db.Blogs)
-            //    {
-            //        Console.WriteLine(blog.Name);
-            //    }
-            //}
-
-
-            //2. The migrations are run even we do not run the context. Seems that this approach fits better to installers.
-            //var configuration = new Migrations.Configuration();
-            //var migrator = new DbMigrator(configuration);
-            //migrator.Update();
-
-            DecompressDatabaseMigration decompress = new DecompressDatabaseMigration();
-            decompress.DecompressModel(
-                "201704120821095_AddBlogUrl",
-                @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=EF6_CodeFirst.BloggingContext;Integrated Security=True");
         }
     }
 }
