2b31ee726471bafa2b63b90929bb6ba71b5c2f0e
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-04-13 08:38:46 +0200

    Added compress function

--- EF_6/EF6_CodeFirst/EF6_CodeFirst/DecompressDatabaseMigration.cs
+++ EF_6/EF6_CodeFirst/EF6_CodeFirst/DecompressDatabaseMigration.cs
@@ -12,6 +12,34 @@ namespace EF6_CodeFirst
 {
     public class DecompressDatabaseMigration
     {
+        public void CompressModel(String migrationName, string connString, string filePath)
+        {
+            var content = File.ReadAllText(filePath);
+            var xDoc = XDocument.Parse(content);
+            byte[] modelBinary = null;
+
+            using (var outStream = new MemoryStream())
+            {
+                using (var gzipStream = new GZipStream(outStream, CompressionMode.Compress))
+                {
+                    xDoc.Save(gzipStream);
+                }
+                modelBinary = outStream.ToArray();
+            }
+
+            using (var connection = new SqlConnection(connString))
+            {
+                connection.Open();
+
+                var sqlToExecute = "UPDATE [EF6_CodeFirst.BloggingContext].[dbo].[__MigrationHistory] set [Model] = @Model where [MigrationId] = @migrationId";
+
+                var command = new SqlCommand(sqlToExecute, connection);
+                command.Parameters.AddWithValue("@Model", modelBinary);
+                command.Parameters.AddWithValue("@migrationId", migrationName);
+                command.ExecuteNonQuery();       
+            }
+        }
+
         public void DecompressModel(String migrationName, string connString)
         {
             var sqlToExecute = String.Format("select model from __MigrationHistory where migrationId like '%{0}'", migrationName);
